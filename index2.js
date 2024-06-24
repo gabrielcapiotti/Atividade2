06a0b3ab6ffe9c3fb8b02305bf229ce57a712b1a
gabrielcapiotti <gabrielcapiottialfa@gmail.com>
2024-06-24 16:53:44 -0300

    .

--- index2.js
+++ index2.js
@@ -3,7 +3,7 @@ const api = axios.create({
 });
 
 async function login(event) {
-  event.preventDefault(); // Evita o comportamento padrão do formulário de submeter a página
+  event.preventDefault();
 
   try {
     const email = document.getElementById("floatingInput").value;
@@ -11,7 +11,7 @@ async function login(event) {
 
     const response = await api.post("login", { email, password });
 
-    console.log(response.data); // Exemplo: Se a API retornar um token, você pode armazená-lo ou manipulá-lo aqui
+    console.log(response.data);
 
     // Redirecionar para outra página ou realizar ações após o login
     alert("Login realizado com sucesso!");
--- signup.js
+++ signup.js
@@ -3,7 +3,7 @@ const api = axios.create({
 });
 
 async function signup(event) {
-  event.preventDefault(); // Evita o comportamento padrão do formulário de submeter a página
+  event.preventDefault();
 
   try {
     const email = document.getElementById("floatingInput").value;
@@ -12,7 +12,7 @@ async function signup(event) {
 
     const response = await api.post("signup", { email, password, name });
 
-    console.log(response.data); // Exemplo: Se a API retornar um objeto com o usuário cadastrado, você pode manipulá-lo aqui
+    console.log(response.data);
 
     // Redirecionar para outra página ou realizar ações após o cadastro
     alert("Cadastro realizado com sucesso!");
